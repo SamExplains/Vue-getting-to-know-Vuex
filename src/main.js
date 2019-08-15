24e6b99bdf14ca0aea57d0a7484e38ca9e46784f
Sampuccino <Sampuccino@gmail.com>
2019-08-14 20:50:48 -0700

    Updated original app with Vuex store. More to add or use.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { store } from "../store/store";
 
 new Vue({
   el: '#app',
+  store, // shortcut in ES6 for variable/property
   render: h => h(App)
-})
+});
